f5cbab88b014b252f4c72c9a3d71d4d4ea3164e5
manu1303 <manuco1303@hotmail.com>
2025-10-17 13:56:46 -0500

    Inicializacion del proyecto WisePick

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 @NgModule({
   declarations: [
     App
@@ -18,3 +21,17 @@ import { App } from './app';
   bootstrap: [App]
 })
 export class AppModule { }
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfo5W4qBrKFqxADo56N8sJRVfGPN4a07c",
+  authDomain: "datashift-c74c4.firebaseapp.com",
+  projectId: "datashift-c74c4",
+  storageBucket: "datashift-c74c4.firebasestorage.app",
+  messagingSenderId: "796933136438",
+  appId: "1:796933136438:web:bc378dca2880c2f3cd7897",
+  measurementId: "G-MSYTYDCK9Y"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
